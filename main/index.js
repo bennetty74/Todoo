83d17b4bb56b5e14b5d8e0c1737207faae98fe64
bugkit <bennetty74@gmail.com>
2025-02-12 22:51:00 +0800

    init

--- main/index.js
+++ main/index.js
@@ -26,7 +26,7 @@ function createWindow() {
     mainWindow.loadFile(staticPath);
   }
 
-  mainWindow.webContents.openDevTools()
+//   mainWindow.webContents.openDevTools()
 
   // 监听来自 Next.js 的任务提醒
   ipcMain.on('send-task-reminder', (event, task) => {
